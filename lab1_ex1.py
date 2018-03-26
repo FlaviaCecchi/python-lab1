849e5328fe7b673c5758981777de847c064e5df9
FlaviaCecchi <flavia.cecchi@studenti.polito.it>
2018-03-26 11:52:08 +0200

    First lab

--- lab1_ex1.py
+++ lab1_ex1.py
@@ -1,3 +1,15 @@
 a = int(input("Insert the first number: "))
 b = int(input("Insert the second number: "))
 print("The sum is", a + b)
+
+# SOLUTION
+
+first_number = ""
+second_number = ""
+print("Hi, I will sum two integer numbers...")
+while not first_number.isdigit():
+    first_number = input("Please, insert a number: ")
+while not second_number.isdigit():
+    second_number = input("Now, insert another number: ")
+result = int(first_number) + int(second_number)
+print(first_number, "+", second_number, "is equal to", result)
--- lab1_ex2.py
+++ lab1_ex2.py
@@ -5,3 +5,13 @@ if len(str) < 3:
 else:
     newstr = str[0] + str[1] + str[len(str)-2] + str[len(str)-1]
     print(newstr)
+
+# SOLUTION
+
+if __name__ == '__main__':
+    string = input("Insert a string: ")
+    if len(string) > 2:
+        result = string[:2] + string[-2:]
+    else:
+        result = ""
+    print("'" + string + "' yields '" + result + "'")
